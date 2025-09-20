ebbf4f6b2381b0864545ed909c498c58b483d965
Sergio Cova <covascode@gmail.com>
2025-09-20 20:34:24 -0300

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()->create([
+            'name' => 'Store A',
+            'email' => 'admin@storea.com',
+            'password' => Hash::make('12345678')
+        ]);
+
+        User::factory()->create([
+            'name' => 'Store B',
+            'email' => 'admin@storeb.com',
+            'password' => Hash::make('12345678')
+        ]);
+
+        User::factory()->create([
+            'name' => 'Store C',
+            'email' => 'admin@storec.com',
+            'password' => Hash::make('12345678')
+        ]);
+    }
+}
